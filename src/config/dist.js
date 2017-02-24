87a4f6c76c80b6f5291d7560f6357195346ee86b
江玲华 <jlh@cang.com>
2017-02-24 15:50:30 +0800

    update
    Signed-off-by: 江玲华 <jlh@cang.com>

--- src/config/dist.js
+++ src/config/dist.js
@@ -4,7 +4,7 @@ import baseConfig from './base';
 
 let config = {
   appEnv: 'dist',  // feel free to remove the appEnv property here
-  apiDomain: ''
+  apiDomain: '' //线上环境api域名
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
